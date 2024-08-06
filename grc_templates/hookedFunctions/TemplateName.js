0015f603d0903b6537283c2978d2b8502722d3a2
BidoumDev <73828942+BidoumDev@users.noreply.github.com>
2024-08-06 12:36:30 +0200

    Ajout de TemplateName un peu partout parce que c'est super !

---
+++ grc_templates/hookedFunctions/TemplateName.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './TemplateName.module.css';
+const templateNameInitialState=false;
+
+const TemplateName = (props) => {     // Arrow function
+  const [state, setstate] = useState(templateNameInitialState);
+
+  useEffect(() => {
+    // Mount && unmlount
+  }, [state]);
+
+  useEffect(() => {
+    // mount
+    return () => {
+      // unmount
+    };
+  });
+
+  return (
+    <div className={styles.TemplateName} data-testid="TemplateName">
+      TemplateName Component<br />{JSON.stringify(state)}
+    </div>
+  );
+};
+
+TemplateName.propTypes = {};
+
+TemplateName.defaultProps = {};
+
+export default TemplateName;
