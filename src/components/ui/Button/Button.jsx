3fd6b3199485e2dd04c3356e3c8c1642d22a646c
BidoumDev <73828942+BidoumDev@users.noreply.github.com>
2024-08-06 09:54:27 +0200

    Avec rafraichissement de la page

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './Button.module.css'
 import PropTypes from 'prop-types'
 
 const Button = (props) => {
-    let isClicked = false;
-    console.log(isClicked, props);
+    //let isClicked = false;
+    //console.log(isClicked, props);
+    const [isClicked, setIsClicked] = useState(false)
     return (
         <>
-        <div>isClicked:{isClicked}</div>
+        <div>isClicked:{isClicked? 'Oui': 'Non'}</div>
         <div 
             style={{...props.style,backgroundColor: props.bgColor}} 
             className={style.Button} 
             data-testid="Button"
+            onClick={(evt)=>{
+                setIsClicked(!isClicked);
+                //isClicked != isClicked;
+                console.log("isClicked", isClicked, evt);
+            }}
         >
             {props.children}
         </div>
         </>
     );
 }
+
 Button.propTypes={
     bgColor: PropTypes.string,
     color: PropTypes.oneOf(['white','black']),
     style: PropTypes.exact({
         textDecoration: PropTypes.oneOf(['underline', 'none']),
         fontWeight: PropTypes.oneOf([100, 500, 900])
-    })
-    
+    })    
 }
+
 Button.defaultProps = {
     bgColor: "yellow"
 }
+
 export default Button;
